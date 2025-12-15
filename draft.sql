b109265dd307e6a81639f765a87581fe1c12c2f4
Juvin <hdcqW@outlook.com>
2025-12-15 18:16:37 +0800

    上传一点测试脚本

--- draft.sql
+++ draft.sql
@@ -1,1 +1,14 @@
+-- 这里放一些常用的查询语句，需要用的时候可以直接copy，方便一点
+
+-- 这个是删除数据库，用于重建
 DROP DATABASE IF EXISTS rainhub_db; 
+
+-- 查看数据库中的内容
+use rainhub_db;
+show tables;
+
+-- 查询四个表的语句
+select * from raingear;
+select * from users;
+select * from station;
+select * from record;
---
+++ test_sql.sql
@@ -0,0 +1,5 @@
+use rainhub_db;
+show tables;
+select * from users;
+select * from station;
+select * from record;
